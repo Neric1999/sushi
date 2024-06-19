d94eeee0a48c52e3dff1f203568c40f31eb92735
Highview Tech Group <highview@Highviews-MacBook-Pro.local>
2024-06-19 08:39:33 +0200

    Adding more meals of sushi

--- lib/views/screens/home.screen.dart
+++ lib/views/screens/home.screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sushi/repo/data/dummy.data.dart';
 import 'package:sushi/views/widgets/category.button.widget.dart';
 import 'package:sushi/views/widgets/center.text.widget.dart';
 import 'package:sushi/views/widgets/food.card.widget.dart';
@@ -14,122 +15,6 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   String selectedCategory = 'Burger';
-
-  final Map<String, List<Map<String, String>>> foodItems = {
-    'Burger': [
-      {
-        'imagePath': 'assets/images/image 26.png',
-        'title': 'Chillox Burger',
-        'subtitle': 'Burgers Fast Food',
-        'rating': '4.8',
-        'time': '10 min',
-      },
-      {
-        'imagePath': 'assets/images/image 26.png',
-        'title': 'Chillox Burger',
-        'subtitle': 'Burgers Fast Food',
-        'rating': '4.8',
-        'time': '10 min',
-      },
-      {
-        'imagePath': 'assets/images/deliciou.jpeg',
-        'title': 'Delicious Pizza',
-        'subtitle': 'Pizza Fast Food',
-        'rating': '4.7',
-        'time': '15 min',
-      },
-      {
-        'imagePath': 'assets/images/hot.png',
-        'title': 'Hot Pizza',
-        'subtitle': 'Pizza Fast Food',
-        'rating': '4.6',
-        'time': '12 min',
-      },
-    ],
-    'Pizza': [
-      {
-        'imagePath': 'assets/images/p1.jpg',
-        'title': 'Delicious Pizza',
-        'subtitle': 'Pizza Fast Food',
-        'rating': '4.7',
-        'time': '15 min',
-      },
-      {
-        'imagePath': 'assets/images/p3.jpg',
-        'title': 'Hot Pizza',
-        'subtitle': 'Pizza Fast Food',
-        'rating': '4.6',
-        'time': '12 min',
-      },
-      {
-        'imagePath': 'assets/images/p4.jpeg',
-        'title': 'Hot Pizza',
-        'subtitle': 'Pizza Fast Food',
-        'rating': '4.6',
-        'time': '12 min',
-      },
-    ],
-    'Salad': [
-      {
-        'imagePath': 'assets/images/s1.jpg',
-        'title': 'Caesar Salad',
-        'subtitle': 'Healthy Food',
-        'rating': '4.9',
-        'time': '8 min',
-      },
-      {
-        'imagePath': 'assets/images/s2.jpg',
-        'title': 'Greek Salad',
-        'subtitle': 'Healthy Food',
-        'rating': '4.7',
-        'time': '9 min',
-      },
-      {
-        'imagePath': 'assets/images/s3.jpeg',
-        'title': 'Greek Salad',
-        'subtitle': 'Healthy Food',
-        'rating': '4.7',
-        'time': '9 min',
-      },
-      {
-        'imagePath': 'assets/images/s4.jpg',
-        'title': 'Greek Salad',
-        'subtitle': 'Healthy Food',
-        'rating': '4.7',
-        'time': '9 min',
-      },
-    ],
-    'Pork': [
-      {
-        'imagePath': 'assets/images/pork1.jpg',
-        'title': 'BBQ Pork Ribs',
-        'subtitle': 'Grilled Food',
-        'rating': '4.8',
-        'time': '20 min',
-      },
-      {
-        'imagePath': 'assets/images/pork2.jpg',
-        'title': 'Roast Pork',
-        'subtitle': 'Grilled Food',
-        'rating': '4.7',
-        'time': '18 min',
-      },
-      {
-        'imagePath': 'assets/images/pork4.jpg',
-        'title': 'Roast Pork',
-        'subtitle': 'Grilled Food',
-        'rating': '4.7',
-        'time': '18 min',
-      },
-      {
-        'imagePath': 'assets/images/pork3.jpg',
-        'title': 'Roast Pork',
-        'subtitle': 'Grilled Food',
-        'rating': '4.7',
-        'time': '18 min',
-      },
-    ],
-  };
 
   void selectCategory(String category) {
     setState(() {
--- lib/views/widgets/food.card.widget.dart
+++ lib/views/widgets/food.card.widget.dart
@@ -20,47 +20,79 @@ class FoodCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Material(
-        elevation: 5,
-        color: Colors.white,
-        shadowColor: const Color.fromARGB(255, 237, 87, 41),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(20.0),
-                topRight: Radius.circular(20.0),
+      child: InkWell(
+        onTap: () {},
+        child: Material(
+          elevation: 5,
+          color: Colors.white,
+          shadowColor: const Color.fromARGB(255, 237, 87, 41),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                ),
+                child: Image.asset(
+                  imagePath,
+                  width: double.infinity,
+                  height: 150.0,
+                  fit: BoxFit.cover,
+                ),
               ),
-              child: Image.asset(
-                imagePath,
-                width: double.infinity,
-                height: 150.0,
-                fit: BoxFit.cover,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          title,
-                          style: const TextStyle(
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.bold,
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            title,
+                            style: const TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
+                          const SizedBox(height: 4),
+                          Text(
+                            subtitle,
+                            style: TextStyle(
+                              fontSize: 14.0,
+                              color: Colors.grey[600],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Row(
+                          children: [
+                            Image.asset(
+                              'assets/images/Vector (4).png',
+                              width: 14,
+                            ),
+                            const SizedBox(width: 4),
+                            Text(
+                              rating.toString(),
+                              style: const TextStyle(
+                                fontSize: 14.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
                         ),
                         const SizedBox(height: 4),
                         Text(
-                          subtitle,
+                          time,
                           style: TextStyle(
                             fontSize: 14.0,
                             color: Colors.grey[600],
@@ -68,40 +100,11 @@ class FoodCard extends StatelessWidget {
                         ),
                       ],
                     ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Row(
-                        children: [
-                          Image.asset(
-                            'assets/images/Vector (4).png',
-                            width: 14,
-                          ),
-                          const SizedBox(width: 4),
-                          Text(
-                            rating.toString(),
-                            style: const TextStyle(
-                              fontSize: 14.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 4),
-                      Text(
-                        time,
-                        style: TextStyle(
-                          fontSize: 14.0,
-                          color: Colors.grey[600],
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
